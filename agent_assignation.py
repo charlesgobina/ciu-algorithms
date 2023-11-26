4c8c9ab04738c0e6fe7c3c1862c624db9712b89d
charlesgobina <charles.gobina@gmail.com>
2023-11-26 09:30:40 +0100

    agent assignation algorithm

--- agent_assignation.py
+++ agent_assignation.py
@@ -38,8 +38,106 @@ def agent_availability(agent):
     # get current hour
     current_hour = datetime.now().hour
     if (
-    (4 <= current_hour < 12 and "morning" in agent_shift) or
+    (7 <= current_hour < 12 and "morning" in agent_shift) or
     (13 <= current_hour < 19 and "afternoon" in agent_shift)):
         return True
+    
+    return False
 
+
+def agent_assination(request, agent_list):
+    request_coordinantes = {
+        "lat": request.localisation.latitude,
+        "lon": request.localisation.longitude
+    }
+
+    available_agents = [free_agents for free_agents in agent_list if agent_availability(free_agents)]
+    if (len(available_agents) == 0):
+        return "No available agent"
+    
+    # get the available_agent location
+    agent_locations_with_closest_proximity_to_request = []
+    for agent in available_agents:
+        agent_location = {
+            "lat": agent.localisation.latitude,
+            "lon": agent.localisation.longitude
+        }
+        distance = haversine(request_coordinantes["lat"], request_coordinantes["lon"], agent_location["lat"], agent_location["lon"])
+        agent_locations_with_closest_proximity_to_request.append({
+            "agent": agent,
+            "distance": distance
+        })
+
+        # sort the list by distance
+        agent_locations_with_closest_proximity_to_request.sort(key=lambda x: x["distance"])
+
+    return agent_locations_with_closest_proximity_to_request[0]
+
+    
+
+# OOP version
+
+class AgentAvailability:
+    def __init__(self, agent):
+        self.agent = agent
+    
+    def is_available(self):
+        current_day = datetime.now().weekday()
+        agent_shift = self.agent.agent_shift[current_day]
+
+        if (agent_shift == None  or len(agent_shift) == 0 or self.agent.agent_availability == False):
+            return False
+        
+        # get current hour
+        current_hour = datetime.now().hour
+        if (
+        (7 <= current_hour < 12 and "morning" in agent_shift) or
+        (13 <= current_hour < 19 and "afternoon" in agent_shift)):
+            return True
+        
+        return False
+    
+    def get_agent(self):
+        return self.agent
+
+
+class AgentAssignation:
+    def __init__(self, request, agent_list):
+        self.request = request
+        self.agent_list = agent_list
+    
+    def get_request_coordinates(self):
+        return {
+            "lat": self.request.localisation.latitude,
+            "lon": self.request.localisation.longitude
+        }
+    
+    def get_available_agents(self):
+        available_agents = [free_agents for free_agents in self.agent_list if AgentAvailability(free_agents).is_available()]
+        return available_agents
+    
+    def get_agent_locations_with_closest_proximity_to_request(self):
+        request_coordinantes = self.get_request_coordinates()
+        available_agents = self.get_available_agents()
+
+        agent_locations_with_closest_proximity_to_request = []
+        for agent in available_agents:
+            agent_location = {
+                "lat": agent.localisation.latitude,
+                "lon": agent.localisation.longitude
+            }
+            distance = haversine(request_coordinantes["lat"], request_coordinantes["lon"], agent_location["lat"], agent_location["lon"])
+            agent_locations_with_closest_proximity_to_request.append({
+                "agent": agent,
+                "distance": distance
+            })
+
+            # sort the list by distance
+            agent_locations_with_closest_proximity_to_request.sort(key=lambda x: x["distance"])
+
+        return agent_locations_with_closest_proximity_to_request
+    
+    def get_agent_with_closest_proximity_to_request(self):
+        agent_locations_with_closest_proximity_to_request = self.get_agent_locations_with_closest_proximity_to_request()
+        return agent_locations_with_closest_proximity_to_request[0]["agent"]
   
